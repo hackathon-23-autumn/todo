1f7d0ffe8e360bf4dd179dc1f4c7d3976026d668
Sen <ffix.3000.c@gmail.com>
2023-10-28 22:18:16 +0900

    Sen:ER図出力とUserテーブルにデフォルトユーザ作成

---
+++ next-app/prisma/seed.ts
@@ -0,0 +1,26 @@
+// schema.prismaのUserテーブルにデフォルトユーザを作成する
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      id: "default_user", // デフォルトユーザーID
+      name: "Alice",
+      email: "alice@prisma.io",
+      // 他の必要なデータをここに記述します
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
