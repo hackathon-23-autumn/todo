a74b0be2ad463e77b53499d979b75a4862590823
Sen <ffix.3000.c@gmail.com>
2023-11-03 18:59:33 +0900

    Sen:addTodosとtodolistをcomponents切り出し

--- next-app/src/app/todo/page.tsx
+++ next-app/src/app/todo/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react" // テキスト入力とTodoリストの状態管理
 import { Button } from "react-bootstrap"
+import AddTodo from "@/components/AddTodo"
+import MyTodoList from "@/components/MyTodoList"
+
 
 type Todo = {
   id: string
@@ -13,11 +16,13 @@ const Todo = () => {
   const [text, setText] = useState<string>("")
   const [todos, setTodos] = useState<string[]>([])
 
+  // テキスト入力フィールドに入力された値をテキストの状態に設定する
   const changeText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value)
     console.log(text)
   }
 
+  // テキスト入力フィールドに入力された値をTodoリストに追加する (追加後にテキスト入力フィールドを空にする)
   const addTodos = () => {
     const newTodos = [...todos]
     newTodos.push(text)
@@ -25,12 +30,14 @@ const Todo = () => {
     setText("")
   }
 
+  // Todoリストから選択されたTodoを削除する
   const deleteTodo = (index: number) => {
     const newTodos = [...todos]
     newTodos.splice(index, 1)
     setTodos(newTodos)
   }
 
+  // ページ読み込み時に1度だけ、fetchData関数を呼び出し、TodoリストをAPIから非同期に取得する
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("/api/todos")
@@ -47,31 +54,13 @@ const Todo = () => {
   }, [])
 
   return (
-    <main>
-      <label className="form-label">todo</label>
-      <div>
-        <input
-          type="text"
-          className="form-label"
-          value={text}
-          onChange={changeText}
-        />
-        <Button variant="primary" onClick={addTodos}>
-          add
-        </Button>
-      </div>
-
-      <div>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={todo}>
-              {todo}
-              <Button variant="primary" onClick={() => deleteTodo(index)}>
-                done
-              </Button>
-            </li>
-          ))}
-        </ul>
+    <main className="flex-col py-2 px-4">
+      <label className="form-label text-4xl font-bold ">todo</label>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full py-6 px-8 bg-slate-200 shadow-md rounded-lg">
+          <AddTodo  changeText={changeText} addTodos={addTodos} text={text} />
+          <MyTodoList  todos={todos} deleteTodo={deleteTodo} />
+        </div>
       </div>
     </main>
   )
