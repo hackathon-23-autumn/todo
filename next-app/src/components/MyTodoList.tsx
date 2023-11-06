0085411dcd3c9da668a6e285bbbb2167c44a53c9
Sen <ffix.3000.c@gmail.com>
2023-11-06 21:54:15 +0900

    Sen:complete時の色塗り

--- next-app/src/components/MyTodoList.tsx
+++ next-app/src/components/MyTodoList.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 
-type Props = {
-  todos: string[];
-  deleteTodo: (index: number) => void;
+// Todo型にcompletedプロパティを追加  
+type Todo = {
+  id: string
+  todo: string
+  completed: boolean
 }
 
-const MyTodoList: React.FC<Props> = ({ todos, deleteTodo }) => {
+// changeStatus関数を追加
+type Props = {
+  todos: Todo[];
+  deleteTodo: (index: number) => void;
+  changeStatus: (index: number) => void;
+}
+
+const MyTodoList: React.FC<Props> = ({ todos, deleteTodo, changeStatus }) => {
   return (
     <ul className='space-y-3'>
     {todos.map((todo, index) => (
-      <li className='flex justify-between p-2 bg-white border-l-4 border-blue-500 rounded shadow' key={index}>
-        <span>{todo}</span>
+      // Todo.completed=trueの時、背景色をgrayに、Todo.completed=falseの時、背景色をwhiteにする
+      <li className={`flex justify-between p-2 border-l-4 rounded shadow ${todo.completed ? 'bg-gray-400 border-blue-500' : 'bg-white border-blue-500'}`} key={index}>
+        <span>{todo.todo}</span>
       
         <div>
-        <button className='py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'>Completed</button>
+        <button onClick={() => changeStatus(index)} className={todo.completed ? 'py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800' : 'py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'}>{todo.completed ? 'Good' : 'Completed'}</button>
         <button onClick={() => deleteTodo(index)} className='py-2 px-2 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'>Delete</button>
         </div>
       </li>
